9948cb396bb97fed1f01ace200710c2a3d497791
Helena Pedro <mbeua94@gmail.com>
2024-07-03 07:04:50 +0100

    new commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from "./NavBar.module.css";
-import globalStyles from '../Global/global.module.css';
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={styles.nav}>
-      <Link to="/" className={globalStyles.icon}>
-        <FontAwesomeIcon icon="house" className="fas fa-house" />
-        <span>Home</span>
-      </Link> 
-      <Link to="/projects" className={globalStyles.icon}>
-        <FontAwesomeIcon icon="building" className="fas fa-building" />
-        <span>Projects</span>
-      </Link>
-      <Link to="/card" className={globalStyles.icon}>
-        <FontAwesomeIcon icon="id-card" className="fas fa-id-card" />
-        <span>Card</span>
-      </Link>
-      <Link to="/certificate" className={globalStyles.icon}>
-        <FontAwesomeIcon icon="certificate" className="fas fa-certificate" />
-        <span>Certificate</span>
-      </Link>
-      <a 
-        href="https://linkedin.com/in/josefpedro/" 
-        target="_blank" 
-        rel="noreferrer"
-        className={globalStyles.icon}>
-        <FontAwesomeIcon icon={['fab', 'linkedin']} className="fab fa-linkedin" />
-        <span>LinkedIn</span>
-      </a>
+      <div className={`${styles.hamburger} ${menuOpen ? styles.active : ''}`} onClick={toggleMenu}>
+        <div />
+        <div />
+        <div />
+      </div>
+      <div className={`${styles.menu} ${menuOpen ? styles.show : ''}`}>
+        <Link to="/" className={`${styles.icon} ${location.pathname === '/' ? styles.active : ''}`} onClick={toggleMenu}>
+          <FontAwesomeIcon icon="house" className="fas fa-house" />
+          <span>Home</span>
+        </Link> 
+        <Link to="/projects" className={`${styles.icon} ${location.pathname === '/projects' ? styles.active : ''}`} onClick={toggleMenu}>
+          <FontAwesomeIcon icon="building" className="fas fa-building" />
+          <span>Projects</span>
+        </Link>
+        <Link to="/card" className={`${styles.icon} ${location.pathname === '/card' ? styles.active : ''}`} onClick={toggleMenu}>
+          <FontAwesomeIcon icon="id-card" className="fas fa-id-card" />
+          <span>Card</span>
+        </Link>
+        <Link to="/certificate" className={`${styles.icon} ${location.pathname === '/certificate' ? styles.active : ''}`} onClick={toggleMenu}>
+          <FontAwesomeIcon icon="certificate" className="fas fa-certificate" />
+          <span>Certificate</span>
+        </Link>
+        <a 
+          href="https://linkedin.com/in/josefpedro/" 
+          target="_blank" 
+          rel="noreferrer"
+          className={styles.icon} onClick={toggleMenu}>
+          <FontAwesomeIcon icon={['fab', 'linkedin']} className="fab fa-linkedin" />
+          <span>LinkedIn</span>
+        </a>
+      </div>
     </nav>
   );
 };
