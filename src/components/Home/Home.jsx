4611e810c84fa1f7412394fc261132bdabd155b3
Helena Pedro <mbeua94@gmail.com>
2024-07-07 22:43:09 +0100

    new commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
                         </strong>
                     </p>
                 </header>
-                <Link to="/certificate#technical-skills" className={`${styles.jumplink} ${styles.projectLink}`}>
+                <Link to="/projects" className={`${styles.jumplink} ${styles.projectLink}`}>
                     <span className={`${styles.arrow} fas fa-chevron-right`}></span>
                     <img src="https://pedro-pi.s3.us-east-2.amazonaws.com/me_1.jpg" alt="José Francisco Pedro" className={styles.profileImage} />
                 </Link>
