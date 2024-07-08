8234f3cf96b0ee3e2a3ae188359bb48fdeedced7
Helena Pedro <91161075+helenapedro@users.noreply.github.com>
2024-07-08 13:25:45 +0100

    Add files via upload
    new commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Card.module.css';
 
 const Card = () => {
   return (
-    <div id='main' className={styles.mainContainer}>
+    <div className={styles.mainContainer}>
       <article className={`${styles.panel} ${styles.introPanel}`}>
-        <header className={styles.header}>
+      <header className={styles.header}>
           <h1 className={styles.title}>José Francisco Pedro</h1>
           <p className={styles.description}>
-            Associated A3 Member of The Order of Engineers of Angola (OEA) <br />
-            <b>Interested Area: Construction Management.</b>
+            <b>Associated A3 Member of The Order of Engineers of Angola (OEA)</b>
+          </p>
+          <p className={styles.description}>
+            <i className={styles.b}>Interested Area:</i>
+            <b> Construction Management.</b>
           </p>
         </header>
-        <a className={`${styles.imageWrapper} ${styles.fitImage} col-2 col-3-medium col-10-small`}>
-          <img src="https://pedro-pi.s3.us-east-2.amazonaws.com/oea.jpeg" alt="Cartão de Membro" className={styles.image} />
-        </a>
+        <a
+            href="https://ambrosiopublicfiles.s3.us-east-2.amazonaws.com/Jose_Pedro_Engineering_Order_Card.jpg"
+            target="_blank"
+            className={`${styles['col-2']} ${styles['col-3-medium']} ${styles['col-10-small']} ${styles.image} ${styles.fit}`}>
+            <img src="https://pedro-pi.s3.us-east-2.amazonaws.com/oea.jpeg" alt=""/>
+          </a>
       </article>
     </div>
   );
