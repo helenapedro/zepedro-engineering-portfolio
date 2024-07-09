2ce59187b66d7bebd3d1116093d237dddaa5ad20
Helena Pedro <mbeua94@gmail.com>
2024-07-09 19:37:28 +0100

    new commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,33 +4,34 @@ import styles from './Card.module.css';
 
 const Card = () => {
   return (
-    <div className={styles.mainContainer}>
-      <article className={`${styles.panel} ${styles.introPanel}`}>
-        <header className={styles.header}>
-          <h2 className={styles.title}>José Francisco Pedro</h2>
-          <p className={styles.description}>
-            <strong>
-              <Link to="/knowledge">Knowledge</Link> in managing and executing civil construction projects,
-              roads, bridges, maritime, and aerodrome pavements.
-            </strong>
+    <div className={`container ${styles.mainContainer}`}>
+      <div className={`card ${styles.introPanel}`}>
+        <div className="card-body">
+          <h2 className={`card-title ${styles['card-title']}`}>José Francisco Pedro</h2>
+          <p className={`card-text ${styles['card-text']}`}>
+            <Link to="/knowledge" className='fst-italic'>Knowledge</Link> in managing and executing 
+            civil construction projects, roads, bridges, maritime, and aerodrome pavements.
           </p>
-          <p className={styles.description}>
-            <strong>
-              <Link to="/skills">Skilled</Link> in using engineering architecture and project management 
-              software to enhance project delivery and quality assurance.
-            </strong>
+          <p className={`card-text ${styles['card-text']}`}>
+            <Link to="/skills" className='fst-italic'>Skilled</Link> in using 
+            engineering architecture and project management 
+            software to enhance project delivery and quality assurance.
           </p>
-        </header>
+        </div>
         <a
           href="https://ambrosiopublicfiles.s3.us-east-2.amazonaws.com/Jose_Pedro_Engineering_Order_Card.jpg"
           target="_blank"
           rel="noopener noreferrer"
-          className={`${styles['col-2']} ${styles['col-3-medium']} ${styles['col-10-small']} ${styles.image} ${styles.fit}`}
+          className="d-block mx-auto mt-3"
         >
-          <i>Associated A3 Member of The Order of Engineers of Angola (OEA)</i>
-          <img src="https://pedro-pi.s3.us-east-2.amazonaws.com/oea.jpeg" alt="OEA"/>
+          <img 
+            src="https://pedro-pi.s3.us-east-2.amazonaws.com/oea.jpeg" 
+            alt="OEA" 
+            className="img-fluid rounded"
+          />
+          <p className="text-center mt-2">Associated A3 Member of The Order of Engineers of Angola (OEA)</p>
         </a>
-      </article>
+      </div>
     </div>
   );
 };
