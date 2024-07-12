65de8a63a728675ed78de162a3ccf475d84a9cd5
Helena Pedro <mbeua94@gmail.com>
2024-07-12 19:33:16 +0100

    new commit

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,8 @@
 const config = {
     baseUrl: "https://dh09x5tu10bt3.cloudfront.net/graduation/",
     projectsUrl: "https://dh09x5tu10bt3.cloudfront.net/",
-    trainingUrl: "https://dh09x5tu10bt3.cloudfront.net/certificates/"
+    trainingUrl: "https://dh09x5tu10bt3.cloudfront.net/certificates/",
+    projectsDataUrl: "https://dh09x5tu10bt3.cloudfront.net/data/projectsData.json"
 };
 
 export default config;
