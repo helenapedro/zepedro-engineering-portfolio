c82bca5228f8371b0ab964099f3b90148b89be03
Helena Pedro <mbeua94@gmail.com>
2024-12-24 17:44:08 +0100

    new commit

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -3,38 +3,44 @@ import useData from '../../pages/Hooks/useData';
 import EducationStyles from '../Education.module.css';
 import styles from './AdditionalTraining.module.css';
 import { wrapNumbersWithClass } from '../../utils/WrapNumbers';
+import { Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Certificates = () => {
     const collectionName = 'training';
     const { data, loading, error } = useData(collectionName); 
 
+    const renderCertificates = (data, index) => (
+        data.images.map((image, imgIndex) => {
+          const imageUrl = `${image}`;
+          return (
+            <Col className='col-6' key={imgIndex}>
+              <a className={EducationStyles.image} href={imageUrl} target="_blank" rel="noopener noreferrer">
+                <img src={imageUrl} alt={`Certificate ${index + 1}`} />
+              </a>
+              <h4 className={styles.title}>
+                {wrapNumbersWithClass(data.title[imgIndex], styles.number)}
+              </h4>
+            </Col>
+          );
+        })
+      );
+
     return (
         <div className={EducationStyles.education}>
-                {loading && <p>Loading...</p>}
-                {error && <p>Error loading data.</p>}
-                {!error && data && Array.isArray(data) && (
-                    data.map((data, index) => (
+            {loading && <p>Loading...</p>}
+            {error && <p>Error loading data.</p>}
+            {!error && data && Array.isArray(data) && (
+                data.map((data, index) => (
                     <article className={EducationStyles.panel} key={index}>
                         <h1>CERTIFICATES</h1>
-                        <div className={EducationStyles.row}>
+                        <Row className={EducationStyles.row}>
                             {data.images && data.images.length > 0 ? (
-                                data.images.map((image, imgIndex) => {
-                                    const imageUrl = `${image}`;
-                                    return (
-                                        <div className='col-6' key={imgIndex}>
-                                            <a className={EducationStyles.image} href={imageUrl} target="_blank" rel="noopener noreferrer">
-                                                <img src={imageUrl} alt={`Certificate ${index + 1}`} />
-                                            </a>
-                                            <h4 className={styles.title}>{wrapNumbersWithClass(data.title[imgIndex], styles.number)}</h4>
-                                        </div>
-                                    );
-                                }) 
+                                renderCertificates(data, index)
                             ) : (
-                                <div className='col-12-small'>
-                                    <p className={styles.title}>{data.title}</p>
-                                </div>
+                                <p>No certificates available.</p>
                             )}
-                        </div>
+                        </Row>
                     </article>
                 ))
             )}
