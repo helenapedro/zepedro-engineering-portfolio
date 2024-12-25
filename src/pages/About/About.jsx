c796c357fe554f84b31eec686e7cf6cc5a508851
Helena Pedro <mbeua94@gmail.com>
2024-12-25 13:04:37 +0100

    new commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
                 <p>{data.descriptionOne}</p>
                 <p>{data.descriptionTwo}</p>
                 <p>{wrapNumbersWithClass(data.descriptionThree, 'number')}</p>
-                <p>{wrapNumbersWithClass(data.descriptionFour, 'number')}</p>
+                {/* <p>{wrapNumbersWithClass(data.descriptionFour, 'number')}</p> */}
                 <a href={data.titleLink} target="_blank" rel="noopener noreferrer" className="btn btn-secondary">OEA Card link</a>
               </section>
             </div>
