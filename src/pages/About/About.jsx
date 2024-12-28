f8a26aba0ba02a4245d03410c331ec946eb84180
Helena Pedro <mbeua94@gmail.com>
2024-12-28 02:40:20 +0100

    new commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useData from '../Hooks/useData';
 import styles from './About.module.css';
 import { wrapNumbersWithClass } from './../../utils/WrapNumbers';
@@ -14,22 +15,30 @@ const About = () => {
       <Row className='card border-0 mb-3'>
         <Col className='row g-0'>
           <div className={`col-md-6 order-1 order-md-2 ${styles.image}`}>
-            <h1 className={styles.name}>{data.name}</h1>
             {data.image && (
               <img src={data.image} alt={data.name} />
             )}
+            <h1 className={styles.name}>{data.name}</h1>
             <h5 className={`${styles['subtitle']} card-subtitle mb-1`}>{data.title}</h5>
-            <a className={styles['title-oea']} href={data.titleLink} target="_blank" rel="noopener noreferrer">
+            {/* <a className={styles['title-oea']} href={data.titleLink} target="_blank" rel="noopener noreferrer">
               {data.titleOEA}
+            </a> */}
+            <a 
+              href={data.titleLink} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="btn btn-secondary"
+            >
+                OEA Member Card link
             </a>
           </div>
+
           <div className="col-md-6 order-2 order-md-1">
             <div className='card-body'>
               <section className={`${styles['card-text']}`}>
                 <p>{data.descriptionOne}</p>
                 <p>{data.descriptionTwo}</p>
                 <p>{wrapNumbersWithClass(data.descriptionThree, 'number')}</p>
-                <a href={data.titleLink} target="_blank" rel="noopener noreferrer" className="btn btn-secondary">OEA Card link</a>
               </section>
             </div>
           </div>
