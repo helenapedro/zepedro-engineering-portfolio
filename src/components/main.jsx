198e1f6854f4aa980555de5b040dcb5ff9736631
Helena Pedro <mbeua94@gmail.com>
2024-12-28 01:00:00 +0100

    new commit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Main.module.css';
 
 const Main = ({ children }) => {
-    return <div className={`${styles.main}`}>{children}</div>;
+    return (
+        <div className={`${styles.main} ${styles['measurement-border']}`}>
+            {children}
+        </div>
+    );
 };
 
 export default Main;
