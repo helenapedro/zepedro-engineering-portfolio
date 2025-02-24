8cf0e7180302f148e315df84d694dd011745a642
Helena Pedro <mbeua94@gmail.com>
2025-02-24 21:14:11 +0100

    Update hero styles

--- src/pages/Home/OwnerIntroduction.jsx
+++ src/pages/Home/OwnerIntroduction.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
+import config from '../../config';
+import StatusBadge from './StatusBadge';
 import { Row, Col, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaEnvelope, FaFileAlt, FaLinkedin } from 'react-icons/fa';
 import { SiWhatsapp } from 'react-icons/si';
-import styles from '../../styles/Home.module.css';
-import config from '../../config';
-import StatusBadge from './StatusBadge';
-import numberstyles from '../../components/ui/Number.module.css'
+import herostyles from '../../components/ui/Hero.module.css';
+import imagestyles from '../../components/ui/Image.module.css';
+import containerstyles from '../../components/ui/Container.module.css';
 
 const OwnerIntroduction = ({ ownerData }) => {
     return (
-        <Card className={`${styles.home} shadow-sm mb-4 border-0 rounded-4`}>
+        <Card className={`${containerstyles.container} mb-4`}>
             <Card.Body className={` p-4`}>
                 <Row className="align-items-center">
                     {/* Profile Image */}
-                    <Col md={4} className="text-center mb-0 mb-md-0">
+                    <Col md={4} className="text-center">
                         {ownerData.mainImage && (
                             <img
                                 src={ownerData.mainImage}
                                 alt={ownerData.name}
-                                className="img-fluid rounded-circle shadow-lg overflow-hidden"
-                                style={{ maxWidth: '180px', border: '2px solid #ddd' }}
+                                className={`${imagestyles.heroImage}`}
                             />
                         )}
-                        <Card.Title as="h2" className="mt-3 text-primary fw-bold">
+                        <Card.Title as="h2" className={`${herostyles.fullName} fw-bold`}>
                             {ownerData.name}
                         </Card.Title>
                         {/* <p className="text-xl text-gray-200 mb-6">Construction Engineer</p> */}
@@ -36,18 +36,18 @@ const OwnerIntroduction = ({ ownerData }) => {
                     </Col>
 
                     {/* Owner Details */}
-                    <Col md={8} className="text-md-start text-center">
-                        <Card.Text className={`mb-2 ${styles['home-text']}`}>
+                    <Col md={8} className={`${herostyles.text}`}>
+                        <Card.Text>
                             <strong>Hello, my name is </strong>
-                            <Link to="/about" className="text-decoration-none text-primary fw-bold">
+                            <Link to="/about" className={`${herostyles.name}`}>
                                 ZéPedro
                             </Link>
                             , an experienced Construction Engineer with over{' '}
-                            <strong className={numberstyles.number}>{ownerData.experienceYears}</strong> years in project
+                            <strong className={`${herostyles.experienceYear} ${herostyles.colorTwo}`}>{ownerData.experienceYears}</strong> years in project
                             management and {' '}
-                            <strong className={numberstyles.number}>{ownerData.qhseExperienceYears}+</strong> year as Coordinator
+                            <strong className={`${herostyles.experienceYear} ${herostyles.colorTwo}`}>{ownerData.qhseExperienceYears}+</strong> year as Coordinator
                             of QHSE-Quality at{' '}
-                            <Link to={ownerData.motaEngilLink} target="_blank" rel="noopener noreferrer" className="fw-bold text-primary">
+                            <Link to={ownerData.motaEngilLink} target="_blank" rel="noopener noreferrer" className={herostyles.colorTwo}>
                                 Mota-Engil Angola
                             </Link>.
                         </Card.Text>
